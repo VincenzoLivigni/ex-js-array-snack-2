578c998bcb0f09cbc9d41df43df08d45561bfafd
VincenzoLivigni <enzolivigni13@gmail.com>
2026-02-10 23:45:33 +0100

    snack2 done

--- script.js
+++ script.js
@@ -53,4 +53,37 @@ console.log("Libri che hanno più di 300 pagine", longBooks)
 
 // ✔️ - Creare un array(longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
 const longBooksTitles = longBooks.map((book) => book.title)
-console.log("Titoli dei libri che hanno più di 300pagine", longBooksTitles)
+console.log("Titoli dei libri che hanno più di 300 pagine", longBooksTitles)
+
+
+
+
+// ✔️ - Snack 2 - Il primo libro scontato
+
+// ✔️ - Creare un array(availableBooks) che contiene tutti i libri disponibili.
+const availableBooks = books.filter((book) => book.available)
+console.log("Libri disponibili", availableBooks)
+
+// ✔️ - Crea un array(discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20 % (mantieni lo stesso formato e arrotonda al centesimo)
+const discountedBooks = availableBooks.map((book) => {
+    const bookPrice = parseFloat(book.price)
+    const priceDiscount = (bookPrice * 0.8).toFixed(2)
+
+    return {
+        ...book, price: `${priceDiscount}€`
+    }
+})
+console.log("Libri sconttai", discountedBooks)
+
+// ✔️ - Salva in una variabile(fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero(senza centesimi).
+const fullPricedBook = discountedBooks.find((book) => {
+    const bookPrice = parseFloat(book.price)
+    return bookPrice === Math.trunc(bookPrice)
+})
+
+if (fullPricedBook) {
+    console.log("Primo libro scontato che ha il prezzo inteo", {
+        ...fullPricedBook, price: `${parseInt(fullPricedBook.price)}€`
+    })
+
+}
