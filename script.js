721f93319c5e67ab15afc64f435bb225d27ca54c
VincenzoLivigni <enzolivigni13@gmail.com>
2026-05-11 15:39:24 +0200

    Snack bonus1 done

--- script.js
+++ script.js
@@ -123,3 +123,41 @@ const agesSum = authors.reduce((acc, a) => acc + a.age, 0)
 // ✔️ - Stampa in console l’età media degli autori dei libri.
 const avgAge = agesSum / authors.length
 console.log("Età media degli autori", avgAge)
+
+
+// Snack 6 (Bonus) - Ordina i libri
+
+/*
+Usando la l'API http://localhost:3333/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks)
+che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+Testala con l’array[2, 13, 7, 21, 19].
+*/
+
+// Crea una variabile booleana(areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
+// Crea un array(booksByPrice) con gli elementi di books ordinati in base al prezzo(crescente).
+//Ordina l’array booksByPricein base alla disponibilità(prima quelli disponibili), senza creare un nuovo array.
+
+const ids = [2, 13, 7, 21, 19]
+
+async function getBooks(ids) {
+
+    const url = "http://localhost:3333/books/"
+
+    const bookPromises = ids.map((id) =>
+        fetch(`${url}${id}`)
+            .then(res => res.json()
+            )
+            .catch(err => {
+                console.log("Error!", err)
+                return null
+            }
+            ))
+
+    const books = await Promise.all(bookPromises)
+
+    return books
+}
+
+getBooks(ids)
+    .then(books => console.log(books))
+    .catch(err => console.log(err))
