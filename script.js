be711152941511df17b565ec808437413bd12ac7
VincenzoLivigni <enzolivigni13@gmail.com>
2026-05-11 15:57:22 +0200

    Snack bonus2 done

--- script.js
+++ script.js
@@ -125,17 +125,13 @@ const avgAge = agesSum / authors.length
 console.log("Età media degli autori", avgAge)
 
 
-// Snack 6 (Bonus) - Ordina i libri
+// Snack 5 (Bonus) - Raccogli i libri 
 
 /*
 Usando la l'API http://localhost:3333/books/{id} usa la combinazione di .map() e Promise.all(), per creare una funzione (getBooks)
 che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
 Testala con l’array[2, 13, 7, 21, 19].
 */
-
-// Crea una variabile booleana(areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
-// Crea un array(booksByPrice) con gli elementi di books ordinati in base al prezzo(crescente).
-//Ordina l’array booksByPricein base alla disponibilità(prima quelli disponibili), senza creare un nuovo array.
 
 const ids = [2, 13, 7, 21, 19]
 
@@ -161,3 +157,20 @@ async function getBooks(ids) {
 getBooks(ids)
     .then(books => console.log(books))
     .catch(err => console.log(err))
+
+
+// Snack 6 (Bonus 2) - Ordina i libri
+
+// Crea una variabile booleana(areThereAvailableBooks) per verificare se c’è almeno un libro disponibile.
+
+const areThereAvailableBooks = books.some((b) => b.available)
+console.log(areThereAvailableBooks);
+
+// Crea un array(booksByPrice) con gli elementi di books ordinati in base al prezzo(crescente).
+const booksByPrice = [...books].sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
+console.log(booksByPrice);
+
+
+// Ordina l’array booksByPrice in base alla disponibilità(prima quelli disponibili), senza creare un nuovo array.
+booksByPrice.sort((a, b) => b.available - a.available)
+console.log(booksByPrice);
